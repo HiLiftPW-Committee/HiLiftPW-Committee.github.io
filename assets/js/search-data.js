32a4b76d9b9f1bb67a534347b4d7d5fd02eeb6cb
AdamClark20 <92387580+AdamClark20@users.noreply.github.com>
2025-09-29 17:01:54 +0000

    Deploying to gh-pages from @ HiLiftPW-Committee/redesign@255bd6b33f03ac952c84364fc37d327ae149c158 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -65,18 +65,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/redesign/publications/";
           },
-        },{id: "post-a-post-with-plotly-js",
-        
-          title: "a post with plotly.js",
-        
-        description: "this is what included plotly.js code could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/redesign/blog/2025/plotly/";
-          
-        },
-      },{id: "post-a-post-with-image-galleries",
+        },{id: "post-a-post-with-image-galleries",
         
           title: "a post with image galleries",
         
