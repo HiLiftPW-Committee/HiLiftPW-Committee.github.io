81442fc481f4452a6d098a42f4f8dddde438059d
AdamClark20 <92387580+AdamClark20@users.noreply.github.com>
2025-10-22 21:18:01 +0000

    Deploying to gh-pages from @ HiLiftPW-Committee/redesign@ff82c4e37b140ee8f8f72bb9569fe98b37f86e21 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -108,8 +108,8 @@ ninja.data = [{
             window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
           
         },
-      },{id: "news-i-sure-don-t-know-what-i-m-doing",
-          title: 'I sure don’t know what I’m doing',
+      },{id: "news-updated-website-launched",
+          title: 'Updated website launched',
           description: "",
           section: "News",},{
         id: 'social-email',
