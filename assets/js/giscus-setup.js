82ec0701a670494f186673251888b8f6f1114e37
AdamClark20 <AdamClark20@users.noreply.github.com>
2025-10-22 21:45:11 +0000

    Deploying to gh-pages from @ HiLiftPW-Committee/hiliftpw-committee.github.io@16132cd8224660db66caa789fabbb322e812ba56 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "HiLiftPW-Committee/redesign",
+    "data-repo": "HiLiftPW-Committee/hiliftpw-committee.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
