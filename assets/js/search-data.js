8ee2fcfa647c5c11e839b0dad38e58f86820a376
AdamClark20 <92387580+AdamClark20@users.noreply.github.com>
2026-01-06 19:04:32 +0000

    Deploying to gh-pages from @ HiLiftPW-Committee/HiLiftPW-Committee.github.io@4eab1fba2d18bc9b346782f52eaeb7187fdb99ad 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -100,8 +100,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "news-hlpw-6-kickoff-meeting-scheduled-for-tuesday-january-20th-7am-pacific-calendar-invite",
-          title: 'HLPW-6 Kickoff meeting scheduled for Tuesday January 20th, 7am Pacific. Calendar Invite.',
+        },{id: "news-hlpw-6-kickoff-meeting-scheduled-for-tuesday-january-20th-7am-pacific-calendar-invitation",
+          title: 'HLPW-6 Kickoff meeting scheduled for Tuesday January 20th, 7am Pacific.  Calendar Invitation',
           description: "",
           section: "News",},{id: "news-updated-website-launched",
           title: 'Updated website launched',
